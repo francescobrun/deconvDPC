102515805473389d8e996752b9da079e46d0a2fb
giada <giadarizzo01@gmail.com>
2025-01-13 15:47:55 +0100

    optimal parameters changed

--- deconvDPC/simulation.py
+++ deconvDPC/simulation.py
@@ -13,7 +13,9 @@ nVoxelZ = 256 #256
 nVoxelY = 256 #128
 nVoxelX = 256 #64
 P = shepp_logan_3d(size_out=[nVoxelZ,nVoxelY,nVoxelX], phantom_type="toft-schabel")
-P= P*10
+path="D:\\FromPEPItoGiada\\deconvDPC\\phantom_simulation\\3dSL\\"
+for i in range(P.shape[0]):
+    tifffile.imwrite(path+ "slice_"+ str(i).zfill(4) + ".tif", P[i,:,:].astype(np.float32)) 
 
 ## Simulate CT:
 n_proj = 300;
@@ -25,10 +27,17 @@ for i in range(P.shape[0]):
     s_i = radon(P[i,:,:], theta=theta, circle=(True))
     dset[i,:,:] = s_i
 
+path="D:\\FromPEPItoGiada\\deconvDPC\\phantom_simulation\\CT_simulation\\"
+for i in range(dset.shape[2]):
+    tifffile.imwrite(path+ "slice_"+ str(i).zfill(4) + ".tif", dset[:,:,i].astype(np.float32)) 
+
 # Prepare differential kernel:
 psf = np.zeros((3, 3))
 psf[1,1] = 1
 psf[1,2] = -1
+
+path_diff="D:\\FromPEPItoGiada\\deconvDPC\\phantom_simulation\\differential_simulation\\"
+path_noise="D:\\FromPEPItoGiada\\deconvDPC\\phantom_simulation\\differential_noise_simulation\\"
 
 # Add gaussian noise:
 mean=0
@@ -40,7 +49,9 @@ dset_d = np.zeros( dset.shape )
 for i in range(dset.shape[2]):
     gaussian_noise=np.random.normal(mean,sd,size)
     dset_d[:,:,i] = convolve2d(dset[:,:,i], psf, 'same')
+    tifffile.imwrite(path_diff+ "slice_"+ str(i).zfill(4) + ".tif", dset_d[:,:,i].astype(np.float32))
     dset_d[:,:,i] = dset_d[:,:,i] + gaussian_noise
+    tifffile.imwrite(path_noise+ "slice_"+ str(i).zfill(4) + ".tif", dset_d[:,:,i].astype(np.float32))
 
 # mask
 center_x, center_y = dset_d.shape[0]// 2, dset_d.shape[1]// 2  
@@ -51,69 +62,94 @@ mask = distance_from_center >= radius
 
 
 # HILBERT reconstruction 
-path_output="D:\\FromPEPItoGiada\\deconvDPC\\hilbert_simul_recon\\"
+path_output="D:\\FromPEPItoGiada\\deconvDPC\\phantom_simulation\\hilbert_simul\\"
 rec = np.zeros( (dset.shape[0],dset.shape[1],dset.shape[1]) )
 for i in range(dset.shape[0]):      
     sino_i = np.imag(hilbert(dset_d[i,:,:], axis=0)) 
     rec_i  = iradon(sino_i, theta=theta, filter_name=None)
-    rec_i = 10*(rec_i-0.007)/(10-0.007)
-    rec_i[mask] = 0
     rec[i,:,:] = rec_i   
-    tifffile.imwrite(path_output + "slice_"+ str(i).zfill(4) + ".tif", rec[i,:,:].astype(np.float32)) 
+
+rec_complete=rec[127,:,:]
+minimum= np.mean(rec_complete[111:121,89:105])
+maximum= np.mean(rec_complete[237:244,118:138])
+
+for i in range(dset.shape[0]):
+    rec[i,:,:] = (rec[i,:,:]-minimum)/(maximum-minimum)
+    rec_i=rec[i,:,:]
+    rec_i[mask] = 0
+    tifffile.imwrite(path_output + "slice_"+ str(i).zfill(4) + ".tif", rec_i.astype(np.float32)) 
 
     
 ## TV deconvolution
 
-path_output="D:\\FromPEPItoGiada\\deconvDPC\\tv_simul_recon\\"
+path_output="D:\\FromPEPItoGiada\\deconvDPC\\phantom_simulation\\tv_simul\\param_0.09\\"
 dset_tv=np.zeros((dset_d.shape))
 
 for i in range(dset_d.shape[2]):
     image=dset_d[:,:,i]   
-    Dw_tv = lib_deconv.tv_deconvolution(image, psf, 0.005, 400) 
+    Dw_tv = lib_deconv.tv_deconvolution(image, psf, 0.09, 400) 
     dset_tv[:,:,i]=Dw_tv
     
 rec=np.zeros( (dset_tv.shape[0],dset_tv.shape[1],dset_tv.shape[1]) )
 for i in range(dset_tv.shape[0]):    
     rec_i  = iradon(dset_tv[i,:,:], theta=theta, filter_name='ramp')
     rec[i,:,:] = rec_i
-    rec_i = 10*(rec_i-0.007)/(10-0.007)
-    rec_i[mask] = 0
-    tifffile.imwrite(path_output + "param_0.005\\slice_"+ str(i).zfill(4) + ".tif", rec[i,:,:].astype(np.float32))   
+ 
+rec_i=rec[127,:,:]
+minimum= np.mean(rec_i[111:121,89:105])
+maximum= np.mean(rec_i[237:244,118:138])
+
+for i in range(rec.shape[0]):
+    rec[i,:,:]= (rec[i,:,:]-minimum)/(maximum-minimum)
+    rec[i,:,:][mask] = 0
+    tifffile.imwrite(path_output + "slice_"+ str(i).zfill(4) + ".tif", rec[i,:,:].astype(np.float32)) 
 
 
 # SPARSE deconvolution
 
-path_output="D:\\FromPEPItoGiada\\deconvDPC\\sparse_simul_recon\\"
+path_output="D:\\FromPEPItoGiada\\deconvDPC\\phantom_simulation\\sparse_simul\\param_0.22\\"
 dset_sparse=np.zeros((dset_d.shape))
 
 for i in range(dset_d.shape[2]):
     image=dset_d[:,:,i]   
-    Dw_sparse = lib_deconv.deconvSps(image, psf, 0.07, 400) 
+    Dw_sparse = lib_deconv.deconvSps(image, psf, 0.22, 400) 
     dset_sparse[:,:,i]=Dw_sparse
     
 rec=np.zeros( (dset_sparse.shape[0],dset_sparse.shape[1],dset_sparse.shape[1]) )
 for i in range(dset_sparse.shape[0]):    
     rec_i  = iradon(dset_sparse[i,:,:], theta=theta, filter_name='ramp')
-    rec_i = 10*(rec_i-0.007)/(10-0.007)
-    rec_i[mask] = 0
-    rec[i,:,:] = rec_i
-    tifffile.imwrite(path_output + "param_0.07\\slice_"+ str(i).zfill(4) + ".tif", rec[i,:,:].astype(np.float32))   
+    rec[i,:,:] = rec_i 
+
+rec_i=rec[127,:,:]
+minimum= np.mean(rec_i[111:121,89:105])
+maximum= np.mean(rec_i[237:244,118:138])
+
+for i in range(rec.shape[0]):
+    rec[i,:,:]= (rec[i,:,:]-minimum)/(maximum-minimum)
+    rec[i,:,:][mask] = 0
+    tifffile.imwrite(path_output + "slice_"+ str(i).zfill(4) + ".tif", rec[i,:,:].astype(np.float32)) 
 
 
 # WIENER deconvolution
 
 dset_wiener=np.zeros((dset_d.shape))
-path_output="D:\\FromPEPItoGiada\\deconvDPC\\wiener_simul_recon\\"
+path_output="D:\\FromPEPItoGiada\\deconvDPC\\phantom_simulation\\wiener_simul\\param_0.00001\\"
 
 for i in range(dset_d.shape[2]):
     image=dset_d[:,:,i]   
-    Dw_wiener = lib_deconv.wiener_deconvolution_fb(image, psf, 0.0000001) 
+    Dw_wiener = lib_deconv.wiener_deconvolution_fb(image, psf, 0.00001) 
     dset_wiener[:,:,i]=Dw_wiener
     
 rec=np.zeros( (dset_wiener.shape[0],dset_wiener.shape[1],dset_wiener.shape[1]) )
 for i in range(dset_wiener.shape[0]):    
     rec_i  = iradon(dset_wiener[i,:,:], theta=theta, filter_name='ramp')
-    rec_i = 10*(rec_i-0.007)/(10-0.007)
-    rec_i[mask] = 0
     rec[i,:,:] = rec_i
-    tifffile.imwrite(path_output + "slice_"+ str(i).zfill(4) + ".tif", rec[i,:,:].astype(np.float32))   
+
+rec_i=rec[127,:,:]
+minimum= np.mean(rec_i[111:121,89:105])
+maximum= np.mean(rec_i[237:244,118:138])
+
+for i in range(rec.shape[0]):
+    rec[i,:,:]= (rec[i,:,:]-minimum)/(maximum-minimum)
+    rec[i,:,:][mask] = 0
+    tifffile.imwrite(path_output + "slice_"+ str(i).zfill(4) + ".tif", rec[i,:,:].astype(np.float32)) 
